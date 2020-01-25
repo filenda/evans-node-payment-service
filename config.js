a7f7eb96ef2cc6c398a2d9490e6dd1275a6053ee
Vinícius Rodrigues Filenga <vinifilenga@hotmail.com>
2020-01-25 12:17:35 -0300

    fixing master branch

---
+++ config.js
@@ -0,0 +1,21 @@
+const stage = process.env.stage;
+const resourcesStage = process.env.resourcesStage;
+const adminPhoneNumber = "+55 11 966113204";
+
+const stageConfigs = {
+  dev: {
+    stripeKeyName: "/stripeSecretKey/test"
+  },
+  prod: {
+    stripeKeyName: "/stripeSecretKey/live"
+  }
+};
+
+const config = stageConfigs[stage] || stageConfigs.dev;
+
+export default {
+  stage,
+  resourcesStage,
+  adminPhoneNumber,
+  ...config
+};
